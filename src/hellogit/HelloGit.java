c4e2b9981fd206209f5acad37ecad713e83688da
Sherly <ss70020@student.uph.edu>
2018-01-16 20:13:17 +0700

    change input

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -16,7 +16,7 @@ public class HelloGit {
      */
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.print("What is your name? ");
+        System.out.print("What is your name?");
         String name=input.nextLine();
         System.out.println("Hello "+name);
         
