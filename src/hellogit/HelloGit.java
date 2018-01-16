95e393734412031a024becb199c5a79255780f19
Sherly <ss70020@student.uph.edu>
2018-01-16 18:19:35 +0700

    Revert "Revert "Ask user for their name""
    This reverts commit 3373c0a005771f7702f4e1cf20090167fe62b8b4.

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package hellogit;
-
+import java.util.Scanner;
 /**
  *
  * @author Student
@@ -15,7 +15,11 @@ public class HelloGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input your name : ");
+        String name=input.nextLine();
+        System.out.println("Hello "+name);
+        
     }
     
 }
